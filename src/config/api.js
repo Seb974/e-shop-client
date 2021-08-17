f74d71bc19d3b9f54dcccbe8b52d48420f0277b0
seb974 <m_seb@icloud.com>
2021-08-17 17:00:43 +0200

    Api config updated to fit with ENV variable

--- src/config/api.js
+++ src/config/api.js
@@ -2,8 +2,12 @@ import axios from 'axios';
 
 // const API_DOMAIN = "http://localhost:8000";
 // const MERCURE_DOMAIN = "http://localhost:3000";
-const API_DOMAIN = "https://api.clikeat.fr";
-const MERCURE_DOMAIN = "https://api.clikeat.fr:3001";
+// const API_DOMAIN = "https://api.clikeat.fr";
+// const MERCURE_DOMAIN = "https://api.clikeat.fr:3001";
+
+const ENV = ( !process.env.NODE_ENV || process.env.NODE_ENV === "development") ? "development" : "production" ;
+const API_DOMAIN = ENV === "development" ? "http://localhost:8000" : "https://api.clikeat.fr";
+const MERCURE_DOMAIN = ENV === "development" ? "http://localhost:3000" : "https://api.clikeat.fr:3001";
 
 function get(route) {
     return axios.get(API_DOMAIN + route);
