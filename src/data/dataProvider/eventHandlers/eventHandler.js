8911010afa59d64793a142ed40215e4889131867
seb974 <m_seb@icloud.com>
2022-02-11 20:20:37 +0400

    Mercure products updates changed to take care of multiple stocks

--- src/data/dataProvider/eventHandlers/eventHandler.js
+++ src/data/dataProvider/eventHandlers/eventHandler.js
@@ -1,22 +1,9 @@
-import categoryEvents from './categoryEvents';
-import productEvents from './productEvents';
 import touringEvents from './touringEvents';
-import userEvents from './userEvents';
 
 function dispatch(event) {
     const data = JSON.parse(event.data);
-    console.log(data);
     if (data['@id'].includes('tourings'))
         touringEvents.update(data);
-
-    // if (data['@id'].includes('products'))
-    //     productEvents.update(data);
-
-    // if (data['@id'].includes('categories'))
-    //     categoryEvents.update(data);
-
-    // if (data['@id'].includes('users') || data['@id'].includes('metas'))
-        // userEvents.update(data);
 }
 
 export default {
